a69c972b58722808abf2b4da052cc9d60b624862
MahmudBekkar <mahmud.bekkar99@gmail.com>
2025-06-03 13:10:51 +0300

    Update test-algorithm.py

--- test-algorithm.py
+++ test-algorithm.py
@@ -1,1 +1,22 @@
 
+import unittest
+from algorithm import run_algorithm
+
+class TestAlgorithm(unittest.TestCase):
+    def test_basic_case(self):
+        input_data = [1, 2, 3, 4, 5]
+        expected_output = [/* your expected output here */]
+        self.assertEqual(run_algorithm(input_data), expected_output)
+
+    def test_empty_input(self):
+        input_data = []
+        expected_output = []
+        self.assertEqual(run_algorithm(input_data), expected_output)
+
+    def test_single_element(self):
+        input_data = [10]
+        expected_output = [/* expected output for single element */]
+        self.assertEqual(run_algorithm(input_data), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()

40e4da0c4c98841913d2f7cd0337c7b24e409a1b
MahmudBekkar <mahmud.bekkar99@gmail.com>
2025-06-15 11:43:21 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,33 +1,11 @@
-def validate_input(data):
-    """
-    Validate the input data for the algorithm.
-    Return True if valid, else False.
-    """
-    if data is None:
-        return False
-    if not isinstance(data, list):
-        return False
-    return True
+import matplotlib.pyplot as plt
+import streamlit as st
 
-def print_debug(message):
-    """
-    Utility function to print debug messages.
-    """
-    print(f"[DEBUG]: {message}")
-
-def load_json(filename):
-    """
-    Load JSON data from a file.
-    """
-    import json
-    with open(filename, 'r') as f:
-        return json.load(f)
-
-def save_json(data, filename):
-    """
-    Save data to a JSON file.
-    """
-    import json
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
+def plot_loss_curve(losses):
+    fig, ax = plt.subplots()
+    ax.plot(losses)
+    ax.set_title("Loss over Epochs")
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Loss")
+    st.pyplot(fig)
 

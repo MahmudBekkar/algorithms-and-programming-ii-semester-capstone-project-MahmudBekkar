3c3876fa82b714cd5c7c3739d4b6ad82eebefdc3
MahmudBekkar <mahmud.bekkar99@gmail.com>
2025-06-22 22:08:06 +0300

    Update test-algorithm.py

--- test-algorithm.py
+++ test-algorithm.py
@@ -3,17 +3,12 @@ import numpy as np
 from algorithm import SimpleNeuralNetwork
 
 class TestNN(unittest.TestCase):
-    def test_forward_shape(self):
-        nn = SimpleNeuralNetwork(2, 4, 1)
-        X = np.array([[0, 1]])
-        output = nn.forward(X)
-        self.assertEqual(output.shape, (1, 1))
+    def test_shape(self):
+        X = np.random.rand(10, 2)
+        y = np.random.randint(0, 2, size=(10, 1))
+        model = SimpleNeuralNetwork(2, 5, 1)
+        output = model.predict(X)
+        self.assertEqual(output.shape, (10, 1))
 
-    def test_output_range(self):
-        nn = SimpleNeuralNetwork(2, 4, 1)
-        X = np.array([[1, 1]])
-        output = nn.forward(X)
-        self.assertTrue((output >= 0).all() and (output <= 1).all())
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()

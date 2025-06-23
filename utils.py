040c03f3cca3dcf11cc21d88079f4d10be55a4fe
MahmudBekkar <mahmud.bekkar99@gmail.com>
2025-06-23 09:30:28 +0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,10 +1,6 @@
-from sklearn.datasets import make_moons
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import numpy as np
 
-def generate_data():
-    X, y = make_moons(n_samples=500, noise=0.2, random_state=42)
-    y = y.reshape(-1, 1)
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-    return train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+def get_xor_data():
+    X = np.array([[0,0], [0,1], [1,0], [1,1]])
+    y = np.array([[0], [1], [1], [0]])
+    return X, y
